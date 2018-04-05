0676f5878c9877eee7b10801f86d2f95f2d04ced
Chris McCullough <mcculloughjchris@gmail.com>
2018-04-05 11:01:06 -0400

    initial commit

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package understrap
+ */
+
+?>
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
+
---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+
+wp_enqueue_script( 'bundle', get_template_directory_uri() . '/js/bundle.js', false, '1.1', 'all');
+
+?>
